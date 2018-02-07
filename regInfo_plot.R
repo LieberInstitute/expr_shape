d3f778785f3f165b47c68330ae46ba3ae57bc09a
Amy Peterson <apeter65@jhu.edu>
2018-02-06 21:50:18 -0500

    Added plot name and saved to pdf folder

--- regInfo_plot.R
+++ regInfo_plot.R
@@ -11,6 +11,8 @@ load_info <- function(f) {
 
 regInfo <- do.call(rbind, lapply(files, load_info))
 
+dir.create('pdf', showWarnings = FALSE)
+pdf('pdf/regInfo_plot.pdf')
 ggplot(regInfo, aes(x = cutoff, y = mean, color = tissue)) + geom_point(size = 0.8) + geom_line() + scale_colour_manual(name = "Tissue", breaks = c("hypothalamus", "substantianigra", "amygdala", "hippocampus", "caudatebasalganglia", "nucleusaccumbensbasalganglia", "putamenbasalganglia", "anteriorcingulatecortexba24", "frontalcortexba9", "cortex", "spinalcordcervicalc-1", "brain_outfile"), labels = c("Hypothalamus", "Substantia Nigra", "Amygdala", "Hippocampus", "Basal Ganglia - Caudate", "Basal Ganglia - Nucleus Accumbens", "Basal Ganglia - Putamen", "Anterior Cingulate Cortex", "Frontal Cortex", "Cortex", "Spinal Cord (Cervical C-1)", "Brain"), values = c("#66CC00","#FF6600", "#999999", "#3399CC", "#CC6633", "#FF3333", "#336600", "#9933CC", "#0000FF", "#006699", "#990033", "#FFCC00"))
 
 dev.off()
